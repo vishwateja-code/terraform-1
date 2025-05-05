0cdf96aa8f5bc9db5020b2ca89bb65da5520e229
vishwateja-code <vishwatejathallapalli@gmail.com>
2025-05-05 17:57:37 +0530

    opentofu

--- day-15opentofu/main.tf
+++ day-15opentofu/main.tf
@@ -7,3 +7,16 @@ resource "aws_instance" "question" {
   
   
 }
+Download the installer script:
+Invoke-WebRequest -outfile "install-opentofu.ps1" -uri "https://get.opentofu.org/install-opentofu.ps1"
+
+ Please inspect the downloaded script at this point.
+
+ Run the installer:
+& .\install-opentofu.ps1 -installMethod standalone
+
+Remove the installer:
+Remove-Item install-opentofu.ps1
+If you run into script execution policy issues when running this script, please run Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process before running the installer.
+Note
+The standalone installer verifies the integrity of the downloaded files. You need to install cosign, GnuPG, or disable the integrity verification by using the -skipVerify option.
